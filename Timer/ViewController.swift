00e1500334d167840b5ad6a226d19bfa00483ed5
Sumandeep2111 <sumandeep21111994@gmail.com>
2019-11-01 16:51:58 -0400

    timer is working

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    var timer:Timer?
+    var seconds:Int = 10
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startTimer(_ sender: UIButton) {
+        runTimer()
+    }
+    func runTimer () {
+        guard timer == nil else {return}
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+            self.updateTimer()
+        })
+    }
+    func updateTimer () {
+        if seconds == 0 { return}
+        else{
+            seconds -= 1
+            timeLabel.text = "\(seconds)"
+        }
+    }
+    func stopTimer () {
+        timer?.invalidate()
+        timer = nil
+    }
 }
 
